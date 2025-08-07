dcdcf82ddb41fd2c77912afeaa7b6cb0e7c9a185
Chakin Pattanasombatsakul <chakin.pattanasombatsakul@gmail.com>
2025-08-07 22:40:54 +0700

    learn struct

---
+++ learn_struct/main.go
@@ -0,0 +1,51 @@
+package main
+
+import "fmt"
+
+type contactInfo struct {
+	email   string
+	zipCode int
+}
+
+type person struct {
+	firstName string
+	lastName  string
+	contactInfo
+}
+
+func main() {
+	// alex := person{firstName: "Alex", lastName: "Anderson"}
+	// var alex person
+	// // update the value in side the struct
+	// alex.firstName = "Alex"
+	// alex.lastName = "Anderson"
+
+	// contactInfo
+	jim := person{
+		firstName: "Jim",
+		lastName:  "Party",
+		contactInfo: contactInfo{
+			email:   "jim@mail.com",
+			zipCode: 12345,
+		},
+	}
+
+	//1.1 add jimPointer and &
+	jimPointer := &jim //ex: 00x23ab
+	jimPointer.updateName("Jimmy")
+	jim.print()
+}
+
+//Receiver function for struct
+func (p person) print() {
+	fmt.Printf("%+v", p)
+}
+
+//1.0 Must use the pointer
+//1.2 add * to receiver function and change to (*pointerToPerson)
+//*person = type address ใน RAM
+//*pointerToPerson = value ที่อยู่ใน address ของ RAM
+func (pointerToPerson *person) updateName(newFirstName string) {
+	(*pointerToPerson).firstName = newFirstName
+	//value ที่ -> (*00x23ab).firstName = "ค่าใหม่"
+}
