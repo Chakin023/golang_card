838d2ed544e393515dcb10c03ddcd0aa145d4bee
Chakin Pattanasombatsakul <chakin.pattanasombatsakul@gmail.com>
2025-08-12 23:15:38 +0700

    doing assignment of interfaces in Udemy grider course

---
+++ assignment_interfaces/main.go
@@ -0,0 +1,43 @@
+package main
+
+import "fmt"
+
+type triangle struct {
+	height float64
+	base   float64
+}
+type square struct {
+	sideLength float64
+}
+
+type shape interface {
+	getArea() float64
+}
+
+func main() {
+	tri := triangle{
+		height: 10.0,
+		base:   10.0,
+	}
+
+	squ := square{
+		sideLength: 10.0,
+	}
+
+	printArea(tri)
+	printArea(squ)
+}
+
+func (t triangle) getArea() float64 {
+	area := 0.5 * t.base * t.height
+	return area
+}
+
+func (s square) getArea() float64 {
+	area := s.sideLength * s.sideLength
+	return area
+}
+
+func printArea(sh shape) {
+	fmt.Println(sh.getArea())
+}
